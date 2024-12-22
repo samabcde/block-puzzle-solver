c7ba84fca1a23eafcb6b424c61233a7f53a7df16
Ng Tsz Sum <tszsumng90@gmail.com>
2024-12-22 23:03:02 +0800

    #2: add color to solution

--- src/main/java/com/samabcde/puzzlesolver/solve/Solution.java
+++ src/main/java/com/samabcde/puzzlesolver/solve/Solution.java
@@ -7,8 +7,11 @@ import com.samabcde.puzzlesolver.component.Position;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.util.*;
 import java.util.function.Consumer;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 public class Solution implements Iterable<BlockPosition> {
     private static final String display = "0123456789" +
@@ -23,10 +26,12 @@ public class Solution implements Iterable<BlockPosition> {
     private final Deque<BlockPosition> positionSolutions = new LinkedList<>();
     private final BitSet addedBlocks;
     private long iterateCount;
+    private final Color[] colors;
 
     public Solution(BlockPuzzle blockPuzzle) {
         this.blockPuzzle = blockPuzzle;
         this.addedBlocks = new BitSet(blockPuzzle.blockCount);
+        this.colors = generateColorsRGB(this.blockPuzzle.blockCount);
     }
 
     private char[] getDisplay(int count) {
@@ -37,30 +42,51 @@ public class Solution implements Iterable<BlockPosition> {
     }
 
     void print() {
-        char[][] solutionView = new char[this.blockPuzzle.getPuzzleHeight()][this.blockPuzzle.getPuzzleWidth()];
-        char[] visible = getDisplay(this.blockPuzzle.blockCount);
+        String[][] solutionView = new String[this.blockPuzzle.getPuzzleHeight()][this.blockPuzzle.getPuzzleWidth()];
         for (BlockPosition blockPosition : positionSolutions) {
             Position position = blockPosition.getPosition();
             Block block = blockPosition.getBlock();
             for (int rowIndex = 0; rowIndex < block.getHeight(); rowIndex++) {
                 for (int colIndex = 0; colIndex < block.getWidth(); colIndex++) {
                     if (block.get(rowIndex, colIndex)) {
-                        solutionView[rowIndex + position.y()][colIndex + position.x()] = visible[block.getPriority()];
+                        solutionView[rowIndex + position.y()][colIndex + position.x()] = styledPoint(block.getPriority());
                     }
                 }
             }
         }
-        for (char[] solutionRowView : solutionView) {
+        for (String[] solutionRowView : solutionView) {
             StringBuilder stringBuilder = new StringBuilder();
-            for (char c : solutionRowView) {
-                if (c == 0) {
-                    stringBuilder.append(" ");
-                } else {
-                    stringBuilder.append(c);
-                }
+            for (String c : solutionRowView) {
+                stringBuilder.append(Objects.requireNonNullElse(c, " "));
             }
             logger.info(stringBuilder.toString());
         }
+    }
+
+    private static Color[] generateColorsRGB(int n) {
+        Color[] colors = new Color[n];
+        int cubeRoot = (int) Math.ceil(Math.pow(n, 1.0 / 3.0));
+
+        // Calculate step size for each component
+        int step = cubeRoot > 1 ? 255 / (cubeRoot - 1) : 255;
+
+        int count = 0;
+        for (int r = 0; r <= 255 && count < n; r += step) {
+            for (int g = 0; g <= 255 && count < n; g += step) {
+                for (int b = 0; b <= 255 && count < n; b += step) {
+                    colors[count] = new Color(r, g, b);
+                    count++;
+                }
+            }
+        }
+
+        return colors;
+    }
+
+    private String styledPoint(int priority) {
+        char[] visible = getDisplay(this.blockPuzzle.blockCount);
+        Color color = this.colors[priority];
+        return ansi().bgRgb(color.getRed(), color.getGreen(), color.getBlue()).a(visible[priority]).reset().toString();
     }
 
     public BlockPosition poll() {
